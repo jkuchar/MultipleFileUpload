c91e79102d2441e175ce46a2d533af5c360b03f5
Jan Kuchař <honza@mujserver.net>
2010-02-25 13:34:19 +0000

    - white spaces

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -13,54 +13,53 @@
  *
  * @author     Jan Kuchař
  */
-class HomepagePresenter extends BasePresenter
-{
+class HomepagePresenter extends BasePresenter {
 
-        public function createComponentForm($name) {
-            $form = new AppForm($this, $name);
-            $form->getElementPrototype()->class[] = "ajax";
+	public function createComponentForm($name) {
+		$form = new AppForm($this, $name);
+		$form->getElementPrototype()->class[] = "ajax";
 
-            /*$form->addText("test","Textové políčko")
+		/*$form->addText("test","Textové políčko")
               ->addRule(Form::FILLED, "Textové políčko test musí být vyplněno!");*/
 
-            // Uploadů můžete do formuláře samozdřejmě přidat více, ale zatím je docela nepříjemná validace a jedna chybka v JS
-            $form->addMultipleFileUpload("upload","Soubory")
-                /*->addRule("MultipleFileUpload::validateFilled","Musíte odeslat alespoň jeden soubor!")
+		// Uploadů můžete do formuláře samozdřejmě přidat více, ale zatím je docela nepříjemná validace a jedna chybka v JS
+		$form->addMultipleFileUpload("upload","Soubory")
+			/*->addRule("MultipleFileUpload::validateFilled","Musíte odeslat alespoň jeden soubor!")
                 ->addRule("MultipleFileUpload::validateFileSize","Soubory jsou dohromady moc veliké!",100*1024)*/;
 
-            $form->addSubmit("odeslat", "Odeslat");
-            $form->onSubmit[] = array($this,"onSubmit");
+		$form->addSubmit("odeslat", "Odeslat");
+		$form->onSubmit[] = array($this,"onSubmit");
 
-            // Invalidace snippetů
-            $form->onInvalidSubmit[] = array($this,"handlePrekresliForm");
-            $form->onSubmit[] = array($this,"handlePrekresliForm");
-        }
+		// Invalidace snippetů
+		$form->onInvalidSubmit[] = array($this,"handlePrekresliForm");
+		$form->onSubmit[] = array($this,"handlePrekresliForm");
+	}
 
-        public function onSubmit(Form $form) {
-            $data = $form->getValues();
+	public function onSubmit(Form $form) {
+		$data = $form->getValues();
 
-            // Předáme data do šablony
-            $this->template->values = $data;
+		// Předáme data do šablony
+		$this->template->values = $data;
 
-            $queueId = uniqid();
+		$queueId = uniqid();
 
-            // Přesumene uploadované soubory
-            foreach($data["upload"] AS $file){
-                // $file je instance HttpUploadedFile
-		$newFilePath = APP_DIR."/uploadedData/q{".$queueId."}__f{".rand(10,99)."}__".$file->getName();
+		// Přesumene uploadované soubory
+		foreach($data["upload"] AS $file) {
+			// $file je instance HttpUploadedFile
+			$newFilePath = APP_DIR."/uploadedData/q{".$queueId."}__f{".rand(10,99)."}__".$file->getName();
 
-		// V produkčním módu nepřesunujeme soubory...
-		if(!Environment::isProduction()) {
-			if($file->move($newFilePath))
-			    $this->flashMessage("Soubor ".$file->getName() . " byl úspěšně přesunut!");
-			else
-			    $this->flashMessage("Při přesouvání souboru ".$file->getName() . " nastala chyba! Pro více informací se podívejte do logů.");
+			// V produkčním módu nepřesunujeme soubory...
+			if(!Environment::isProduction()) {
+				if($file->move($newFilePath))
+					$this->flashMessage("Soubor ".$file->getName() . " byl úspěšně přesunut!");
+				else
+					$this->flashMessage("Při přesouvání souboru ".$file->getName() . " nastala chyba! Pro více informací se podívejte do logů.");
+			}
 		}
-            }
-        }
+	}
 
-        public function handlePrekresliForm() {
-            $this->invalidateControl("form");
-        }
+	public function handlePrekresliForm() {
+		$this->invalidateControl("form");
+	}
 
 }
