74ce2ebef6fbcc3374380c1acc94132f9ccbb2d8
Jan Kuchař <honza@mujserver.net>
2010-03-21 21:08:45 +0000

    - deleted unused parts of the file

--- app/controls/MultipleFileUpload/Models/SQLite/setupDB.sql
+++ app/controls/MultipleFileUpload/Models/SQLite/setupDB.sql
@@ -1,21 +1,6 @@
-/*CREATE TABLE queues (
-	id INTEGER PRIMARY KEY ,
-	queueID CHAR(13) NOT NULL UNIQUE,
-	lastAccessTime INTEGER(11) NOT NULL
-);*/
-
 CREATE TABLE files (
 	id INTEGER PRIMARY KEY ,
 	queueID CHAR(13) NOT NULL ,
 	created INTEGER(11) NOT NULL ,
 	data TEXT NOT NULL
 );
-/*
-CREATE VIEW queues AS 
-  SELECT queueID, created AS lastAccess
-  FROM files
-  GROUP BY queueID
-  ORDER BY lastAccess DESC*/
-
-
-/*INSERT INTO queues (queueID,lastAccessTime) VALUES ("test",1236);*/
