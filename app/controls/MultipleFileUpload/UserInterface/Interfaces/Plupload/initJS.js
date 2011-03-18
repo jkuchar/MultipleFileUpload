8e60e002757034be41065c667bd44a0b4ccb6b61
Jan Kuchař <honza@mujserver.net>
2011-03-18 12:13:02 +0000

    - plupload switched to use all posible interfaces

--- app/controls/MultipleFileUpload/UserInterface/Interfaces/Plupload/initJS.js
+++ app/controls/MultipleFileUpload/UserInterface/Interfaces/Plupload/initJS.js
@@ -1,37 +1,34 @@
 var fallbackController = this;
 (function($){
-// API: http://www.plupload.com/plupload/docs/api/index.html
+	// API: http://www.plupload.com/plupload/docs/api/index.html
 
-// Convert divs to queue widgets when the DOM is ready
+	// Convert divs to queue widgets when the DOM is ready
 	$(function(){
-		//setTimeout(function(){
-			var uploader = $("#"+{!$id|escapeJs}).pluploadQueue({
-				// General settings
-				//runtimes : 'gears,html5,browserplus,silverlight,flash,html4',
-				//runtimes : 'gears,html5,browserplus,silverlight,html4',
-				runtimes : 'flash',
-				url : {!$backLink|escapeJs},
-				max_file_size : {!$sizeLimit},
-				chunk_size : '5mb',
-				/*rename: true,*/
-				//unique_names : true,
+		// TODO: auto fallback
+		var uploader = $("#"+{!$id|escapeJs}).pluploadQueue({
+			// General settings
+			runtimes : 'gears,html5,browserplus,flash,silverlight,html4',
+			{* runtimes : 'gears,html5,browserplus,silverlight,html4', *}
+			{* runtimes : 'flash',*}
+			url : {!$backLink|escapeJs},
+			max_file_size : {!$sizeLimit},
+			chunk_size : '5mb',
 
-				headers: {
-					"X-Uploader": "plupload",
-					"token"     : {!$token|escapeJs}
-				},
+			headers: {
+				"X-Uploader": "plupload",
+				"token"     : {!$token|escapeJs}
+			},
 
-				// Flash settings
-				flash_swf_url : {!$baseUri|escapeJs}+'swf/MultipleFileUpload/plupload/plupload.flash.swf',
+			// Flash settings
+			flash_swf_url : {!$baseUri|escapeJs}+'swf/MultipleFileUpload/plupload/plupload.flash.swf',
 
-				// Silverlight settings
-				silverlight_xap_url : {!$baseUri|escapeJs}+'xap/MultipleFileUpload/plupload/plupload.silverlight.xap'
-			});
-			uploader = $(uploader).pluploadQueue();
-			uploader.bind("Error",function(){
-				fallbackController.fallback();
-			})
-		//},1000);
+			// Silverlight settings
+			silverlight_xap_url : {!$baseUri|escapeJs}+'xap/MultipleFileUpload/plupload/plupload.silverlight.xap'
+		});
+		uploader = $(uploader).pluploadQueue();
+		uploader.bind("Error",function(){
+			fallbackController.fallback();
+		})
 	});
 
 	return true; // OK
