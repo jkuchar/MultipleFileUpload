5830d9b19150d79fbd54e8a6355dcb995a141f76
Ciki <matula.m@gmail.com>
2012-08-13 23:02:22 +0200

    HTML4SingleUpload now renders maxFiles instances of file inputs, not only 5

--- app/components/MultipleFileUpload/UserInterface/Interfaces/HTML4SingleUpload/MFUUIHTML4SingleUpload.php
+++ app/components/MultipleFileUpload/UserInterface/Interfaces/HTML4SingleUpload/MFUUIHTML4SingleUpload.php
@@ -1,6 +1,5 @@
 <?php
-
-/*
+/* 
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -12,76 +11,71 @@
  */
 class MFUUIHTML4SingleUpload extends MFUUIBase {
 
-	/**
-	 * Is this upload your upload? (upload from this interface)
-	 */
-	public function isThisYourUpload() {
-		return !(\Nette\Environment::getHttpRequest()->getHeader('user-agent') === 'Shockwave Flash');
-	}
+  /**
+   * Is this upload your upload? (upload from this interface)
+   */
+  public function isThisYourUpload() {
+    return !(\Nette\Environment::getHttpRequest()->getHeader('user-agent') === 'Shockwave Flash');
+  }
 
-	/**
-	 * Handles uploaded files
-	 * forwards it to model
-	 */
-	public function handleUploads() {
-		// Iterujeme nad přijatými soubory
-		foreach (\Nette\Environment::getHttpRequest()->getFiles() AS $name => $controlValue) {
+  /**
+   * Handles uploaded files
+   * forwards it to model
+   */
+  public function handleUploads() {
+    // Iterujeme nad přijatými soubory
+    foreach(\Nette\Environment::getHttpRequest()->getFiles() AS $name => $controlValue) {
 
-			// MFU vždy posílá soubory v této struktuře:
-			//
+      // MFU vždy posílá soubory v této struktuře:
+      //
       // array(
-			//	"token" => "blablabla",
-			//	"files" => array(
-			//		0 => HttpUploadedFile(...),
-			//		...
-			//	)
-			// )
+      //	"token" => "blablabla",
+      //	"files" => array(
+      //		0 => HttpUploadedFile(...),
+      //		...
+      //	)
+      // )
 
-			$isFormMFU = (
-				is_array($controlValue) and
-					isset($controlValue["files"]) and
-					isset($_POST[$name]["token"])
-			);
+      $isFormMFU = (is_array($controlValue) and isset($controlValue["files"]) and isset($_POST[$name]["token"]));
+      if($isFormMFU) {
+        $token = $_POST[$name]["token"];
+		foreach($controlValue["files"] AS $file) {
+          self::processFile($token,$file);
+        }
+      }
+      // soubory, které se netýkají MFU nezpracujeme -> zpracuje si je standardním způsobem formulář
+    }
+    return true; // Skip all next
+  }
 
-			if($isFormMFU) {
-				$token = $_POST[$name]["token"];
-				foreach ($controlValue["files"] AS $file) {
-					self::processFile($token, $file);
-				}
-			}
-			// soubory, které se netýkají MFU nezpracujeme -> zpracuje si je standardním způsobem formulář
-		}
-		return true; // Skip all next
-	}
+  /**
+   * Renders interface to <div>
+   */
+  public function render(MultipleFileUpload $upload) {
+    $template = $this->createTemplate(dirname(__FILE__)."/html.latte");
+    $template->maxFiles = $upload->maxFiles;
+    $template->mfu = $upload;
+    return $template->__toString(TRUE);
+  }
 
-	/**
-	 * Renders interface to <div>
-	 */
-	public function render(MultipleFileUpload $upload) {
-		$template = $this->createTemplate(dirname(__FILE__) . "/html.latte");
-		$template->mfu = $upload;
-		return $template->__toString(TRUE);
-	}
+  /**
+   * Renders JavaScript body of function.
+   */
+  public function renderInitJavaScript(MultipleFileUpload $upload) {
+  	return $this->createTemplate(dirname(__FILE__)."/initJS.js")->__toString(TRUE);
+  }
 
-	/**
-	 * Renders JavaScript body of function.
-	 */
-	public function renderInitJavaScript(MultipleFileUpload $upload) {
-		return $this->createTemplate(dirname(__FILE__) . "/initJS.js")->__toString(TRUE);
-	}
+  /**
+   * Renders JavaScript body of function.
+   */
+  public function renderDestructJavaScript(MultipleFileUpload $upload) {
+  	return true;
+  }
 
-	/**
-	 * Renders JavaScript body of function.
-	 */
-	public function renderDestructJavaScript(MultipleFileUpload $upload) {
-		return true;
-	}
-
-	/**
-	 * Renders set-up tags to <head> attribute
-	 */
-	public function renderHeadSection() {
-		return "";
-	}
-
+  /**
+   * Renders set-up tags to <head> attribute
+  */
+  public function renderHeadSection() {
+    return "";
+  }
 }
