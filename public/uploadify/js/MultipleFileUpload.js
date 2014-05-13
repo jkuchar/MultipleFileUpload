7d582f682c33597f89b43cef1c050640cbb7b874
Ciki <matula.m@gmail.com>
2014-05-13 03:52:47 +0200

    remove $.livequery dependency

--- public/uploadify/js/MultipleFileUpload.js
+++ public/uploadify/js/MultipleFileUpload.js
@@ -1,18 +1,18 @@
-$("form").livequery("submit",function(e){
+$(document).on('submit', 'form', function(e){
 	var form = $(this);
 	var multipleFileUploadFields = $(".MultipleFileUpload", this);
 	var uploadersInQueue = multipleFileUploadFields.length;
 
-    if(uploadersInQueue>0){
+	if(uploadersInQueue>0){
 		multipleFileUploadFields.each(function(){
 			var uploadify = $(".uploadify[id]",this),
 			queueSize = 0;
-			
+
 			try{
 				queueSize = uploadify.data('uploadify').queueData.queueLength;
 			}catch(ex) {}
-			
-            if(queueSize>0){
+
+			if(queueSize>0){
 				e.stopImmediatePropagation();
 				e.preventDefault();
 				uploadify.uploadify('upload', '*');
@@ -23,10 +23,8 @@ $("form").livequery("submit",function(e){
 					}
 				});
 			} else {
-                uploadersInQueue--;
-            }
+				uploadersInQueue--;
+			}
 		});
 	}
 });
-
-
