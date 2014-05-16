64ac7c8b45d53f582c5b964450d86967dd0070b8
Jan Kuchař <honza.kuchar@grifart.cz>
2014-05-16 16:39:50 +0200

    - plupload updated to version 2.1.2

--- MultipleFileUpload/UI/Plupload/initJS.js
+++ MultipleFileUpload/UI/Plupload/initJS.js
@@ -7,30 +7,27 @@ var fallbackController = this;
 		// TODO: auto fallback
 		var uploader = $("#"+{$id|escapeJs|noescape}).pluploadQueue({
 			// General settings
-			runtimes : 'gears,browserplus,silverlight,flash,html5,html4',
-			{* runtimes : 'gears,html5,browserplus,silverlight,html4', *}
-			{* runtimes : 'flash',*}
+			runtimes : 'html5,flash,silverlight,html4',
 			url : {$uploadLink|escapeJs|noescape},
 			max_file_size : {$sizeLimit|noescape},
 			chunk_size : '5mb',
-			
+
 			// Intentionally do not use headers, because not all interfaces allows you to send them.
 			// insted using parameters in URL or POST
-			
+
 			// Flash settings
-			flash_swf_url : {$interface->baseUrl|escapeJs|noescape}+'/swf/plupload.flash.swf',
+			flash_swf_url : {$interface->baseUrl|escapeJs|noescape}+'/Moxie.swf',
 
 			// Silverlight settings
-			silverlight_xap_url : {$interface->baseUrl|escapeJs|noescape}+'/xap/plupload.silverlight.xap'
+			silverlight_xap_url : {$interface->baseUrl|escapeJs|noescape}+'/Moxie.xap'
 		});
 		uploader = $(uploader).pluploadQueue();
-		console.log(uploader);
 		var refreshFn = function(){ // if plupload moves around page, good to recompute position of uploader
 			uploader.refresh();
 		};
 		setInterval(refreshFn,1000);
 		refreshFn();
-		
+
 		uploader.bind("Error",function(){
 			fallbackController.fallback();
 		})
