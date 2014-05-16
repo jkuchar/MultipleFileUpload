734ecbcf3b65c9030d467b0530dca62cd01a1710
Jan Kuchař <honza.kuchar@grifart.cz>
2014-05-16 16:36:48 +0200

    - SQLite3 driver added header

--- MultipleFileUpload/Model/SQLite3/Queue.php
+++ MultipleFileUpload/Model/SQLite3/Queue.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * This file is part of the MultipleFileUpload (https://github.com/jkuchar/MultipleFileUpload/)
+ *
+ * Copyright (c) 2014 Ciki (https://github.com/Ciki)
+ * Copyright (c) 2014 Jan Kuchař (http://www.jankuchar.cz)
+ *
+ * For the full copyright and license information, please view
+ * the file license.txt that was distributed with this source code.
+ */
+ 
+ 
 namespace MultipleFileUpload\Model\SQLite3;
 
 use MultipleFileUpload\Model\BaseQueue,
